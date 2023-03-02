d333cbd9a23e29dd92f898ee7d4564157dba543d
Ruhanitamanna <tamannanasrin01@gmail.com>
2023-03-03 05:49:25 +0600

    made cards

--- js/script.js
+++ js/script.js
@@ -1,9 +1,30 @@
+// getting url
+
 const gettingUrl = async() =>{
     const url =`https://openapi.programming-hero.com/api/ai/tools`
     const res = await fetch(url);
     const data = await res.json();
-    console.log(data.data);
+    displayTools(data.data.tools);
 
+}
+
+// display tools
+ const displayTools = tools =>{
+   const toolsContainer = document.getElementById('ai-container');
+   tools.forEach( tool => {
+    const toolDiv = document.createElement('div');
+    toolDiv.classList.add('col');
+    toolDiv.innerHTML = `
+    <div class="card">
+                    <img src="..." class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title">Card title</h5>
+                      <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    </div>
+                  </div>`;
+                  
+        toolsContainer.appendChild (toolDiv);           
+   })
 }
 
 gettingUrl()
