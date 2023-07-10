7a644cacde127fbc1b0ebeff5d7ffdd9a109ca79
Ruhanitamanna <tamannanasrin01@gmail.com>
2023-07-10 19:49:57 +0600

    updated

--- js/app.js
+++ js/app.js
@@ -36,6 +36,7 @@ const displayData = (tools) => {
   tools.forEach((tool) => {
     const toolDiv = document.createElement('div');
     toolDiv.classList.add('col');
+    
 
     const featureList = document.createElement('ol');
     for (const feature in tool.features) {
@@ -43,6 +44,7 @@ const displayData = (tools) => {
       const li = document.createElement('li');
       li.textContent = item;
       featureList.appendChild(li);
+     
     }
 
     toolDiv.innerHTML = `
@@ -60,7 +62,7 @@ const displayData = (tools) => {
                 <i class="fa-solid fa-calendar-days"></i>
                 <p class="card-text">${tool.published_in}</p>
               </div>
-              <button onclick="loadToolsDetails(${tool.id})" class="btn btn-outline-danger" data-bs-toggle="modal" data-bs-target="#exampleModal">
+              <button onclick="loadDetails('${tool.id}')" class="btn btn-outline-danger" data-bs-toggle="modal" data-bs-target="#exampleModal">
               <i class="fa-solid fa-arrow-right"></i>
             </button>
             </div>
@@ -86,52 +88,100 @@ const displayData = (tools) => {
 
   // load data for modals
 
-  const loadToolsDetails = (id) => {
-    const url = `https://openapi.programming-hero.com/api/ai/tool?id=${id}`;
-    fetch(url)
-      .then((res) => res.json())
-      .then((data) => {
-        displayDetails(data.data.tool);
-      });
-  };
+  const loadDetails= async id =>{
+   const url=  `https://openapi.programming-hero.com/api/ai/tool/${id}`;
+    
+   const res = await fetch(url);
+   const data = await res.json();
+   displayDetails(data.data);
+    
+
+  }
+ 
 
  // show modal
 
- const displayDetails = (tool) => {
-    const modalBody = document.querySelector('.modal-body');
-    modalBody.innerHTML = '';
+ const displayDetails = item =>{
+  console.log(item)
+  const itemDetails = document.getElementById('item-details');
   
-    const toolDiv = document.createElement('div');
-    toolDiv.innerHTML = `
-      <div class="container width-full d-flex">
-        <div>
-          <p>${tool.description ? tool.description : ''}</p>
-          <div>
-            <div></div>
-            <div></div>
-            <div></div>
+
+  
+
+  itemDetails.innerHTML=`
+  <div class='d-flex p-2'>
+      <div class='boarder boarder-warning rounded p-2'>
+          <h6>${item.description ? item.description : 'No Description'}<h6/>
+          
+          
+          <div class='d-flex justify-content-around m-2'>
+              
+              <div class='border border-warning rounded p-2'>
+               
+                <p>${item.pricing[0].price}</p>
+                <p>${item.pricing[0].plan}</p>
+                
+
+              </div>
+              <div class='border border-warning rounded p-2'>
+                  <p>${item.pricing[1].price}</p>
+                  <p>${item.pricing[1].plan}</p>
+              </div>
+              <div class='border border-warning rounded p-2'>
+                  <p>${item.pricing[2].price}</p>
+                  <p>${item.pricing[2].plan}</p>
+              </div>
+
+
           </div>
-          <ul>
-            ${tool.features.map((feature) => `<li>${feature}</li>`).join('')}
-          </ul>
-        </div>
-        <div>
-          <div class="card">
-            <img src="${tool.image}" class="card-img-top" alt="...">
+
+          <div class='d-flex'>
+            <div>
+            <h4> Features</h4>
+              <ul>
+                <li>${item.use_cases[0].name ? item.use_cases[0].name : 'no data found'}</li>
+                <li>${item.use_cases[1].name ? item.use_cases[1].name : 'no data found'}</li>
+                <li>${item.use_cases[2].name ? item.use_cases[2].name : 'no data found'}</li>
+                
+              </ul>
+            </div>
+            <div>
+            <h4> Integrations </h4>
+               <ul>
+                  <li>${item.integrations[0] ? item.integrations[0] :' no data found'}</li>
+                  <li>${item.integrations[1] ? item.integrations[1] :' no data found'}</li>
+                  <li>${item.integrations[2] ? item.integrations[2] :' no data found'}</li>
+                  <li>${item.integrations[3] ? item.integrations[3] :' no data found'}</li>
+                  <li>${item.integrations[4] ? item.integrations[4] :' no data found'}</li>
+                  
+                </ul>
+            </div>
+
+
+          </div>
+      </div>
+      <div>
+          <div class="card" style="width: 18rem;">
+              <img src=${item.image_link[0] ? item.image_link[0] :'no Image Found'} class="card-img-top" alt="...">
             <div class="card-body">
-              <h3>${tool.name}</h3>
-              <p class="card-text">${tool.published_in}</p>
+              <h5 class="card-title">Hi, how are you doing today?</h5>
+              <p class="card-text">I'm doing well, thank you for asking. How can I assist you today?</p>
+    
             </div>
           </div>
         </div>
-      </div>`;
+  </div>
   
-    modalBody.appendChild(toolDiv);
-  };
-
+  `
+ }
 
 
   
+  
+
+
+
+  loadDetails()
 
    gettingToolsData().catch((error) => {
     console.error(error);
